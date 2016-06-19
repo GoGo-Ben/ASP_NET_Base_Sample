2016bd6b503f39859119e8790b74465c9b8428eb
TTben <benrock666@gmail.com>
2016-06-19 16:57:37 +0800

    二維陣列轉Json

--- Two-dimensional_Arrays_to_Json/Two-dimensional_Arrays_to_Json/Program.cs
+++ Two-dimensional_Arrays_to_Json/Two-dimensional_Arrays_to_Json/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Two_dimensional_Arrays_to_Json
 {
@@ -15,13 +16,13 @@ namespace Two_dimensional_Arrays_to_Json
 
             // loop 
             myArray.Add("1");
-            myArray.Add("2");
+            myArray.Add("5");
             myTwoArray.Add(myArray);
             // end loop
 
             List<string> myArray2 = new List<string>();
-            myArray2.Add("3");
-            myArray2.Add("4");
+            myArray2.Add("10");
+            myArray2.Add("15");
             myTwoArray.Add(myArray2);
             //myTwoArray supposed be  [ [1,2],[3,4] ]
 
@@ -29,16 +30,18 @@ namespace Two_dimensional_Arrays_to_Json
             //Equivalent to myTwoArray[0][0]
             //Equivalent to myTwoArray[0][1]
             List<String> temp = myTwoArray[0];
-            Console.WriteLine(temp[0]);
-            Console.WriteLine(temp[1]);
+            //Console.WriteLine(temp[0]);
+            //Console.WriteLine(temp[1]);
 
             // 2nd times 
             List<String> temp2 = myTwoArray[1];
-            Console.WriteLine(temp2[0]);
-            Console.WriteLine(temp2[1]);
+            //Console.WriteLine(temp2[0]);
+            //Console.WriteLine(temp2[1]);
 
-            //var str_json = JsonConvert.SerializeObject(my2d, Formatting.Indented);
-            //Console.Write(str_json);
+            var str_json = JsonConvert.SerializeObject(myTwoArray);
+            var str_json2 = JsonConvert.SerializeObject(myTwoArray, Formatting.Indented);
+            Console.WriteLine(str_json);
+            Console.WriteLine(str_json2);
             Console.ReadLine();
 
         }
